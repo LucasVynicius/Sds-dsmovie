b51c85ae7e567cd20af1084956389515bee91b64
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2022-03-12 21:21:17 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from 'assets/img/Vector.svg';
+import './style.css';
+
+function Navbar(){
+    return(
+        <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie Pro</h1>
+          <a href="https://github.com/LucasVynicius">
+            <div className='dsmovie-contact-container'>
+              <GithubIcon />
+              <p className='dsmovie-contact-link'>/LucasVynicius</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header> 
+    )
+}
+
+export default Navbar;
